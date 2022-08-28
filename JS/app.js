33a614cefe40727df172a57142ff0670499e1823
nikodittmar <nikodittmar@gmail.com>
2022-08-27 22:44:38 -0700

    view height mobile

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,6 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 $(window).scroll(function() {
     if ($(document).scrollTop() > 45) {
         $('nav').addClass('nav-active');
